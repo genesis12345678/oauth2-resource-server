427bfe9b2575ba2202cb01e267fe01e400e92651
장근영 <jky00914@naver.com>
2024-05-27 10:53:57 +0900

    Scope 기반 권함 매핑

--- src/main/java/io/security/oauth2resourceserver/config/SecurityConfig.java
+++ src/main/java/io/security/oauth2resourceserver/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import com.nimbusds.jose.jwk.RSAKey;
 import io.security.oauth2resourceserver.filter.authentication.JwtAuthenticationFilter;
 import io.security.oauth2resourceserver.filter.authorization.JwtAuthorizationRsaPublicKeyFilter;
 import io.security.oauth2resourceserver.signature.RsaSecuritySigner;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,13 +27,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+    public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+/*
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -45,6 +48,30 @@ public class SecurityConfig {
                 .oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()))
         ;
 
+        return http.build();
+        */
+        http
+                .securityMatchers(matchers -> matchers.requestMatchers("/photos/1"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/photos/1").hasAuthority("SCOPE_photo")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()))
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+
+        http
+                .securityMatchers(matchers -> matchers.requestMatchers("/photos/2"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/photos/2").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()))
+        ;
         return http.build();
     }
 
